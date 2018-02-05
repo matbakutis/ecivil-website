f2122e3df9094cdc4da6e6df3c27e04c73f3e0e6
matbakutis <31048002+matbakutis@users.noreply.github.com>
2018-02-05 12:13:41 -0600

    added carousel

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import AnimatedWrapper from "./AnimatedWrapper";
 import "./Home.css";
+import Carousel from "nuka-carousel";
 
 class HomeComponent extends Component {
 	render() {
 		return (
 			<div className="page">
 				<div className="title">
-					<h1>Home</h1>
+					
+					<Carousel autoplay={true} wrapAround={true} className>
+						<img src="pictures/example1.jpg"/>
+						<img src="pictures/example2.jpg"/>
+						<img src="pictures/example3.jpg"/>
+						<img src="pictures/example4.jpg"/>
+					</Carousel>
 				</div>
 			</div>
 		);
